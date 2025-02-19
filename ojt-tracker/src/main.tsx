f632c05f14e2dd31e07864f6a22262bde7800f6d
Dan Lloyd "Ayato" Cabanilla <139518282+DLAyatoCabanilla@users.noreply.github.com>
2025-02-20 00:56:38 +0800

    Update main.tsx with CompanyList.tsx

--- ojt-tracker/src/main.tsx
+++ ojt-tracker/src/main.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import './index.css'
 import Dashboard from "./pages/Dashboard";
+import CompanyList from "./pages/CompanyList";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/companies" element={<CompanyList />} /> {/* CompanyList */}
     </Routes>
   </BrowserRouter>
 );
