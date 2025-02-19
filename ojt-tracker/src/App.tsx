d49a9c2bbe71af21169584c817c29dcdb19e6d6b
Dan Lloyd "Ayato" Cabanilla <139518282+DLAyatoCabanilla@users.noreply.github.com>
2025-02-20 02:30:06 +0800

    Update App.tsx w/ WeeklyReport Import

--- ojt-tracker/src/App.tsx
+++ ojt-tracker/src/App.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import CompanyList from "./pages/CompanyList";
-import './Appwn.css'
+import './App.css'
+import WeeklyReport from "./pages/WeeklyReport";
+
 
 export default function App() {
   return (
@@ -11,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Login />} /> {/* Login Route */}
         <Route path="/dashboard" element={<Dashboard />} /> {/* Dashboard Route */}
         <Route path="/companies" element={<CompanyList />} /> {/* CompanyList */}
+        <Route path="/weekly-report" element={<WeeklyReport />} /> {/* WeeklyReport */}
       </Routes>
     </Router>
   );
