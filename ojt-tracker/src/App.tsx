3eb7f7aae1a1d876356fd565a52e82ed82f7989d
Dan Lloyd "Ayato" Cabanilla <139518282+DLAyatoCabanilla@users.noreply.github.com>
2025-03-18 21:39:17 +0800

    Update App.tsx

--- ojt-tracker/src/App.tsx
+++ ojt-tracker/src/App.tsx
@@ -1,18 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import CompanyList from "./pages/CompanyList";
 import './App.css'
 import WeeklyReport from "./pages/WeeklyReport";
-
+import LandingPage from "./pages/LandingPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} /> {/* Login Route */}
+        <Route path="/landing-page" element={<LandingPage />} /> {/*  Landing Route */}
         <Route path="/dashboard" element={<Dashboard />} /> {/* Dashboard Route */}
-        <Route path="/companies" element={<CompanyList />} /> {/* CompanyList */}
         <Route path="/weekly-report" element={<WeeklyReport />} /> {/* WeeklyReport */}
       </Routes>
     </Router>
