dbff6c861874e192a9fc9ec54ef5dec41b1651e7
Juliana R. Mancera <mancerajuliana98@gmail.com>
2025-03-26 00:16:18 +0800

    rawr

--- ojt-tracker/src/App.tsx
+++ ojt-tracker/src/App.tsx
@@ -14,7 +14,7 @@ export default function App() {
         <Route path="/landing-page" element={<LandingPage />} /> {/*  Landing Route */}
         <Route path="/dashboard" element={<Dashboard />} /> {/* Dashboard Route */}
         <Route path="/weekly-report" element={<WeeklyReport />} /> {/* WeeklyReport */}
-        <Route path="/application-approval" element={<ApplicationApproval />} /> {/* ApplicationApproval */}
+        
       </Routes>
     </Router>
   );
