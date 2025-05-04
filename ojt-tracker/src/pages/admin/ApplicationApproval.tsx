a7e8f531bc665047967f68cf005548d22539e334
VinceCampos <152839517+VinceCampos@users.noreply.github.com>
2025-05-04 22:48:25 +0800

    Blur Background on Submission

--- ojt-tracker/src/pages/admin/ApplicationApproval.tsx
+++ ojt-tracker/src/pages/admin/ApplicationApproval.tsx
@@ -4,7 +4,7 @@ import { User as SupabaseUser } from "@supabase/supabase-js";
 import Sidebar from "./SideBar";
 import OJTLogo from "/src/assets/ojt-white.png";
 import { Loading } from "../../components/Loading";
-import { Folder, User, Settings, CircleHelp, LogOut, UserCog, ArrowLeft, FileText } from "lucide-react";
+import { Folder, User, Settings, CircleHelp, LogOut, UserCog, ArrowLeft} from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { updateAllJobSlots } from "../../services/JobService";
 
